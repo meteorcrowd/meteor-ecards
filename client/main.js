41844c8c590ffc2f3f70751ea32e040579284192
nizyev <illya.nizyev@gmail.com>
2014-11-29 20:10:47 +0200

    options setted up for inline-edit fields

--- client/main.js
+++ client/main.js
@@ -57,12 +57,61 @@ Template.compose.events({
 Template.compose.helpers({
     'message_to_email_options': function () {
         return {
-            type: 'textarea',
+            type: 'text',
+            emptyText: "Your friend's email",
             //async: true,
             position: 'top',
             value: Session.get('message_to_email'),
             onsubmit: function (value) {
                 Session.set('message_to_email', value);
+            }
+        };
+    },
+    'message_from_name_options': function () {
+        return {
+            type: 'text',
+            emptyText: 'Your name',
+            //async: true,
+            position: 'top',
+            value: Session.get('message_from_name'),
+            onsubmit: function (value) {
+                Session.set('message_from_name', value);
+            }
+        };
+    },
+    'message_to_name_options': function () {
+        return {
+            type: 'text',
+            emptyText: "Your friend's name",
+            //async: true,
+            position: 'top',
+            value: Session.get('message_to_name'),
+            onsubmit: function (value) {
+                Session.set('message_to_name', value);
+            }
+        };
+    },
+    'message_options': function () {
+        return {
+            type: 'textarea',
+            emptyText: 'Your message',
+            //async: true,
+            position: 'right',
+            value: Session.get('message'),
+            onsubmit: function (value) {
+                Session.set('message', value);
+            }
+        };
+    },
+    'message_from_email_options': function () {
+        return {
+            type: 'text',
+            emptyText: 'Your email',
+            //async: true,
+            position: 'top',
+            value: Session.get('message_from_email'),
+            onsubmit: function (value) {
+                Session.set('message_from_email', value);
             }
         };
     }
