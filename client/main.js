a7791c144360e32a6216adbe74d7b6e609ae3a7c
Brylie Christopher Oxley <brylie@gnumedia.org>
2014-11-29 15:47:37 +0200

    functional basics, selection, form, preview

--- client/main.js
+++ client/main.js
@@ -1,38 +1,56 @@
-FS.debug = true;
-var imageStore = new FS.Store.FileSystem('images', {
-    path: "~home/code/Meteor/meteor-ecards/public/images"
-});
-imageCollection = new FS.Collection('images', {
-    stores: [imageStore]
-});
+var Images = [
+        {id: 1, path: "images/Erika.jpg"},
+        {id: 2, path: "images/Earth.jpg"},
+        {id: 3, path: "images/webmaker-logo.png"}
+    ];
 
-imageCollection.allow({
-    insert: function () {
-        return true;
+Template.images.helpers({
+    'images': function () {
+        'use strict';
+        return Images;
     },
-    update: function () {
-        return true;
-    },
-    remove: function () {
-        return true;
-    },
-    download: function () {
-        return true;
+    'selectedClass': function () {
+        'use strict';
+        var imageId = this.id,
+            selectedImage = Session.get('selectedImage');
+        if (imageId === selectedImage) {
+            return "selected";
+        }
     }
 });
-
-if (Meteor.isClient) {
-    Template.uploadForm.events({
-        'click input[type="submit"]': function (event, template) {
-//            var file = $('#file').get(0).files[0];
-//            var fileObj = imageCollection.insert(file);
-//            console.log('Upload result: ', fileObj);
-            FS.Utility.eachFile(event, function(file) {
-                imageCollection.insert(file, function (err, fileObj) {
-                    console.log("Inserted new image");
-                    //Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
-                });
-            });
-        }
-    });
-}
+Template.images.events({
+    'click .image': function (event) {
+        'use strict';
+        console.log(this.id);
+        Session.set('selectedImageId', this.id);
+        Session.set('selectedImagePath', this.path);
+    }
+});
+Template.compose.events({
+    'keyup, change form': function (event) {
+        'use strict';
+       Session.set(event.target.name, event.target.value);
+    }
+});
+Template.preview.helpers({
+    'image': function () {
+        'use strict';
+        return Session.get('selectedImagePath');
+    },
+    'message_to': function () {
+        'use strict';
+        return Session.get('message_to');
+    },
+    'to_email': function () {
+        'use strict';
+        return Session.get('message_to_email');
+    },
+    'message_from': function () {
+        'use strict';
+        return Session.get('message_from');
+    },
+    'message': function () {
+        'use strict';
+        return Session.get('message');
+    }
+});
